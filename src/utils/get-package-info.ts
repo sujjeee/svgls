64d32af586a7c35c3d1ab047c38ffc0517271d9c
Suraj <sujjeee@outlook.com>
2023-12-19 08:51:05 +0530

    updated getPackageInfo

--- src/utils/get-package-info.ts
+++ src/utils/get-package-info.ts
@@ -2,6 +2,6 @@ import packageJson from "package-json";
 import type { PackageJson } from "type-fest";
 
 export async function getPackageInfo() {
-  const packageInfo = await packageJson("dubco");
+  const packageInfo = await packageJson("svgls");
   return packageInfo as PackageJson;
 }
